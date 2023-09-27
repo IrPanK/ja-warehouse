e5d35023e0af647283af1aa8373f50a9740b7d57
IrPanTech <irfannkamill@gmail.com>
2023-09-27 08:55:29 +0700

    Tugas 4

--- main/models.py
+++ main/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
 
@@ -10,3 +12,4 @@ class Item(models.Model):
     price = models.IntegerField()
     category = models.CharField(max_length=255)
     add_date = models.DateField(auto_now_add=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
